1dfbeb2f4437353fa6ec0cc645f5bb0a3d4b52be
user42 <rofernan@student.42.fr>
2020-08-12 16:12:48 +0200

    rm useless files

--- files/allumer.sh
+++
@@ -1,6 +0,0 @@
-if ! minikube status > /dev/null 2>&1
-then
-	echo "Starting Minikube"
-	minikube start --vm-driver=docker --extra-config=apiserver.service-node-port-range=1-35000
-	sudo usermod -aG docker $(whoami)
-fi
