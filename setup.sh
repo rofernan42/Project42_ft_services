72cc72602205c39092fa7f13db8e5e160edf6f40
user42 <rofernan@student.42.fr>
2020-08-13 00:21:22 +0200

    modif update minikube

--- setup.sh
+++ setup.sh
@@ -16,6 +16,10 @@ then
 	exit 1
 fi
 
+# Install last version of Minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube /usr/local/bin
+
 if ! minikube status > /dev/null 2>&1
 then
 	echo "Starting Minikube"
@@ -25,34 +29,36 @@ fi
 
 minikube addons enable metrics-server
 minikube addons enable dashboard
+minikube addons enable metallb
 
 server_ip="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
 
-# Remplacer par IP
+# Replace by IP
 sed -i.bak "s/http:\/\/IP/http:\/\/"$server_ip"/g" srcs/nginx/index.html
 sed -i.bak "s/http:\/\/IP/http:\/\/"$server_ip"/g" srcs/wordpress/wp-config.php
 sed -i.bak "s/http:\/\/IP/http:\/\/"$server_ip"/g" srcs/mysql/wordpress.sql
 sed -i.bak "s/SERVER-IP/"$server_ip"/g" srcs/ftps/my-ftps.sh
-sed -i.bak "s/IP-IP/"$server_ip"-"$server_ip"/g" srcs/my-metallb.yaml
 
 eval $(minikube docker-env)
 
-docker build -t my-nginx ./srcs/nginx --quiet
-docker build -t my-mysql ./srcs/mysql --quiet
-docker build -t my-wordpress ./srcs/wordpress --quiet
-docker build -t my-phpmyadmin ./srcs/phpmyadmin --quiet
-docker build -t my-influxdb ./srcs/influxdb --quiet
-docker build -t my-grafana ./srcs/grafana --quiet
-docker build -t my-telegraf ./srcs/telegraf --quiet
-docker build -t my-ftps ./srcs/ftps --quiet
+# Build Docker images
+docker build -t my-nginx ./srcs/nginx
+docker build -t my-mysql ./srcs/mysql
+docker build -t my-wordpress ./srcs/wordpress
+docker build -t my-phpmyadmin ./srcs/phpmyadmin
+docker build -t my-influxdb ./srcs/influxdb
+docker build -t my-grafana ./srcs/grafana
+docker build -t my-telegraf ./srcs/telegraf
+docker build -t my-ftps ./srcs/ftps
 
-# Remettre a l'etat initial
+# Put back initial settings
 sed -i.bak "s/http:\/\/"$server_ip"/http:\/\/IP/g" srcs/nginx/index.html
 sed -i.bak "s/http:\/\/"$server_ip"/http:\/\/IP/g" srcs/wordpress/wp-config.php
 sed -i.bak "s/http:\/\/"$server_ip"/http:\/\/IP/g" srcs/mysql/wordpress.sql
 sed -i.bak "s/"$server_ip"/SERVER-IP/g" srcs/ftps/my-ftps.sh
-rm srcs/wordpress/wp-config.php.bak srcs/mysql/wordpress.sql.bak srcs/nginx/index.html.bak srcs/ftps/my-ftps.sh.bak srcs/my-metallb.yaml.bak
+rm srcs/wordpress/wp-config.php.bak srcs/mysql/wordpress.sql.bak srcs/nginx/index.html.bak srcs/ftps/my-ftps.sh.bak
 
+# Deploy the apps
 kubectl apply -k ./srcs/
 
 echo "Server IP : $server_ip"
